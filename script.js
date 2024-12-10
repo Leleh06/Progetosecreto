15c8f66a30c987cb70057bf00d6e8fe78e990213
aangellxz <aangellxz2008@gmail.com>
2024-12-09 22:20:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+function equipe(){
+    
+    
+       document.getElementById('equipe').style.display = 'block'
+         document.getElementById('cadastro').style.display = 'none'
+        document.getElementById('editar').style.display = 'none'
+        document.getElementById('deletar').style.display = 'none'
+         document.getElementById('listar').style.display = 'none'
+     }
+    
+    function cadastro(){
+
+        document.getElementById('equipe').style.display = 'none'
+        document.getElementById('cadastro').style.display = 'block'
+        document.getElementById('editar').style.display = 'none'
+        document.getElementById('deletar').style.display = 'none'
+        document.getElementById('listar').style.display = 'none'
+        
+    }
+    
+    
+    
+    
+    
+    function editar(){
+        
+         document.getElementById('equipe').style.display = 'none'
+        document.getElementById('cadastro').style.display = 'none'
+        document.getElementById('editar').style.display = 'block'
+        document.getElementById('deletar').style.display = 'none'
+        document.getElementById('listar').style.display = 'none'
+        
+        
+    }
+    
+        
+    
+    function deletar(){
+        
+         document.getElementById('equipe').style.display = 'none'
+        document.getElementById('cadastro').style.display = 'none'
+        document.getElementById('editar').style.display = 'none'
+        document.getElementById('deletar').style.display = 'block'
+        document.getElementById('listar').style.display = 'none'
+    
+    }function listar(){
+    
+     document.getElementById('equipe').style.display = 'none'
+    document.getElementById('cadastro').style.display = 'none'
+    document.getElementById('editar').style.display = 'none'
+    document.getElementById('deletar').style.display = 'none'
+    document.getElementById('listar').style.display = 'block'
+    }
